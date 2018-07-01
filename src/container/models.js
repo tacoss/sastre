66c822139b36579f6b0bd766d1e5be86cf0e3b14
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2018-07-01 00:17:22 -0500

    Cleanup

--- src/container/models.js
+++ src/container/models.js
@@ -1,32 +1,24 @@
 const Resolver = require('@lib/resolver');
 const Sequelize = require('sequelize');
 
-class ModelDecorator {
-  constructor(sequelize) {
-    this.sequelize = sequelize;
-  }
-
-  before(name, definition) {
-    const { attributes, ...options } = definition;
-
-    return this.sequelize.define(name, attributes || {}, options);
-  }
-
-  after(name, definition) {
-    Object.assign(definition, definition.classMethods);
-    Object.assign(definition.prototype, definition.instanceMethods);
-
-    delete definition.attributes;
-    delete definition.classMethods;
-    delete definition.instanceMethods;
-  }
-}
-
 class ModelsResolver {
   constructor(modelsDir) {
     const sequelize = new Sequelize('sqlite::memory:');
 
-    return new Resolver(modelsDir, new ModelDecorator(sequelize));
+    return new Resolver(modelsDir, {
+      before(name, definition) {
+        const { attributes, ...options } = definition;
+
+        return sequelize.define(name, attributes || {}, options);
+      },
+      after(name, definition) {
+        Object.assign(definition, definition.classMethods);
+        Object.assign(definition.prototype, definition.instanceMethods);
+
+        delete definition.classMethods;
+        delete definition.instanceMethods;
+      },
+    });
   }
 }
 
