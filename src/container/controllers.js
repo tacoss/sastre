b6a34abd5ff4a163f980629ea809915a682ac07e
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2018-07-03 10:10:10 -0500

    Allow use() to receive a custom decorator; fix

--- src/container/controllers.js
+++ src/container/controllers.js
@@ -2,7 +2,9 @@ const Resolver = require('@lib/resolver');
 
 class ControllersResolver {
   constructor(controllersDir, container) {
-    return new Resolver(controllersDir, Resolver.use(container));
+    return new Resolver(controllersDir, Resolver.use(container, (name, definition) => {
+      console.log('Custom logic here, e.g. GRPC IoC (aka front-controller)', name, definition);
+    }));
   }
 }
 
