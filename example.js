303a4e224f596062ea4eb8143854cba93ad97f0d
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2018-07-11 23:23:17 -0500

    Get rid of module-alias/register imports; use -r flags

--- example.js
+++ example.js
@@ -1,5 +1,3 @@
-require('module-alias/register');
-
 const container = require('@src/container');
 
 // standard classes
--- src/api/models/User/classMethods/add/index.test.js
+++ src/api/models/User/classMethods/add/index.test.js
@@ -1,5 +1,3 @@
-require('module-alias/register');
-
 const container = require('@src/container');
 const td = require('testdouble');
 
