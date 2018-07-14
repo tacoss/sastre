7ed1116ea964936e230265792fa916f48cc121de
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2018-07-14 13:26:52 -0500

    Fixed Injector impl; change API

--- test/injector.test.js
+++ test/injector.test.js
@@ -4,23 +4,56 @@ const Injector = require('../lib/injector');
 
 const expect = require('chai').expect;
 
-/* global it, describe */
+/* global it, describe, beforeEach */
 
 describe('Injector', () => {
   describe('constructor', () => {
-    it('just atore given factory and deps', () => {
-      function dummy({ foo }) {
-        return foo;
+    const someInjectables = {
+      dep1() {
+        return 42;
+      },
+    };
+
+    let fakeResolver;
+
+    beforeEach(() => {
+      fakeResolver = {
+        _values: {
+          dep1: {},
+        },
+        _dependencies: {},
+      };
+    });
+
+    it('will return plain functions as is', () => {
+      function test() {
+        return 42;
       }
 
-      const test = new Injector(dummy, {
-        foo() {
-          return 42;
-        },
-      });
+      const testFn = new Injector(test, someInjectables);
+      const result = Injector.bind(fakeResolver, testFn)();
 
-      expect(test.factory).not.to.be.undefined;
-      expect(test.dependencies.foo).not.to.be.undefined;
+      expect(result).to.eql(42);
+    });
+
+    it('will return instantiated classes bound', () => {
+      class Test {
+        constructor({ dep1 }) {
+          this.dep = dep1;
+        }
+      }
+
+      const testClass = new Injector(Test, someInjectables);
+      const result = Injector.bind(fakeResolver, testClass);
+
+      expect(result).to.deep.eql({ dep: 42 });
+    });
+
+    it('arrow functions are used to inject values', () => {
+      const testArrow = new Injector(({ dep1 }) => () => dep1, someInjectables);
+      const result = Injector.bind(fakeResolver, testArrow);
+
+      expect(result()).to.eql(42);
     });
   });
 
@@ -32,7 +65,7 @@ describe('Injector', () => {
 
   describe('supports', () => {
     it('validates if values are injectables', () => {
-      expect(Injector.supports(new Injector())).to.be.true;
+      expect(() => Injector.supports(new Injector())).to.throw('Cannot inject non-callable values');
     });
   });
 
