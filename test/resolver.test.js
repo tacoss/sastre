f413d54eab2c410291146cf2c4d7656f608b0072
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2018-07-15 14:01:55 -0500

    Clean resolver impl; extract container

--- test/resolver.test.js
+++ test/resolver.test.js
@@ -1,66 +1,11 @@
 /* eslint-disable no-unused-expressions */
 
 const Resolver = require('../lib/resolver');
-const Injector = require('../lib/injector');
 
 const td = require('testdouble');
 const expect = require('chai').expect;
 
 /* global it, describe, beforeEach, afterEach */
-
-class ExampleClass {
-  constructor(anyInstance) {
-    this.x = anyInstance;
-  }
-}
-
-const ExampleObject = {
-  test: -1,
-};
-
-function ExampleFunction(value) {
-  return value;
-}
-
-const values = {
-  ExampleClass,
-  ExampleObject,
-  ExampleFunction,
-};
-
-const injectables = {
-  aNumber(container) {
-    return container.aNumber;
-  },
-};
-
-const validInjectables = {
-  getExampleClass() {},
-  getExampleObject() {},
-  getExampleFunction() {},
-};
-
-function mockResolver(registry, fixedValues, fixedDependencies) {
-  return {
-    _dependencies: {
-      ...fixedDependencies,
-    },
-    _decorator: {
-      before: td.func('before'),
-      after: td.func('after'),
-    },
-    _registry: {
-      ...registry,
-    },
-    _values: {
-      ...values,
-      ...fixedValues,
-    },
-    _lock: {},
-    unwrap: Resolver.prototype.unwrap,
-    get: Resolver.prototype.get,
-  };
-}
 
 describe('Resolver', () => {
   describe('static methods', () => {
@@ -113,24 +58,9 @@ describe('Resolver', () => {
       });
 
       it('should work as expected', () => {
-        expect(new Resolver(null, cwd)._registry).to.be.deep.eql({ foo });
+        expect(new Resolver(cwd)._container.registry).to.be.deep.eql({ foo });
+        expect(new Resolver(cwd)._container.values).to.be.deep.eql({ foo: Foo });
       });
-
-      it('can receive a function, it will be used as after-hook', () => {
-        const callback = td.func('after-hook');
-        const container = new Resolver(null, '.', callback);
-
-        expect(container.get('foo')).to.eql(Foo);
-        expect(td.explain(callback).callCount).to.eql(1);
-      });
-    });
-
-    describe('use', () => {
-      it('TODO', () => {});
-    });
-
-    describe('bind', () => {
-      it('TODO', () => {});
     });
 
     describe('loadFile', () => {
@@ -152,77 +82,15 @@ describe('Resolver', () => {
         td.when(Resolver.loadFile('./Name/prop/method/index.js')).thenReturn(function method() {});
         td.when(Resolver.loadFile('./Example/index.js')).thenReturn(class Example {});
 
-        const container = new Resolver(null, '.');
+        const container = new Resolver('.');
 
-        expect(container._decorator.before).not.to.be.undefined;
-        expect(container._decorator.after).not.to.be.undefined;
-        expect(container._values.Example).not.to.be.undefined;
-        expect(container._values.Name).not.to.be.undefined;
-        expect(container._registry.Example).not.to.be.undefined;
-        expect(container._registry.Name.prop.method).not.to.be.undefined;
+        expect(container._decorators.before).not.to.be.undefined;
+        expect(container._decorators.after).not.to.be.undefined;
+        expect(container._container.values.Example).not.to.be.undefined;
+        expect(container._container.values.Name).not.to.be.undefined;
+        expect(container._container.registry.Example).not.to.be.undefined;
+        expect(container._container.registry.Name.prop.method).not.to.be.undefined;
       });
     });
-  });
-
-  it('will fail on missing values', () => {
-    const resolverInstance = mockResolver({
-      ExampleClass: {
-        staticMethod: new Injector(() => function staticMethod() {}, injectables),
-      },
-    });
-
-    expect(() => resolverInstance.get('ExampleClass')).to.throw("Missing 'aNumber' dependency");
-  });
-
-  it('will fail on non-injectable values', () => {
-    const resolverInstance = mockResolver({
-      aNumber: 42,
-    }, {
-      aNumber: 42,
-    });
-
-    expect(() => resolverInstance.get('aNumber')).to.throw("Target 'aNumber' is not an object, given '42'");
-  });
-
-  it('will resolve from rootContainer as fallback', () => {
-    const resolverInstance = mockResolver(null, null, {
-      theTruth() {
-        return 42;
-      },
-    });
-
-    expect(resolverInstance.get('theTruth')()).to.eql(42);
-  });
-
-  it('will resolve injectable values otherwise', () => {
-    const resolverInstance = mockResolver({
-      ExampleClass: {
-        decoratedMethod: new Injector(({ ExampleObject: Eo }) => () => Eo, validInjectables),
-      },
-      ExampleObject: {
-        testString: 'BAR',
-        testFunction: new Injector(({ ExampleClass: Ex }) => () => Ex, validInjectables),
-      },
-      ExampleFunction: {
-        prototype: {
-          testSelf: new Injector(({ ExampleFunction: Fn }) => () => Fn, validInjectables),
-        },
-      },
-    });
-
-    expect(() => resolverInstance.get('ExampleClass').decoratedMethod().testFunction().decoratedMethod()).not.to.throw();
-
-    const exampleClass = new (resolverInstance.get('ExampleClass'))(42);
-
-    expect(exampleClass.x).to.eql(42);
-
-    const Fn = resolverInstance.get('ExampleFunction');
-
-    expect(Fn(42)).to.eql(42);
-
-    expect(new Fn().testSelf()).to.eql(ExampleFunction);
-
-    expect(resolverInstance.get('ExampleObject').testString).to.eql('BAR');
-    expect(resolverInstance.get('ExampleObject').testFunction()).to.eql(ExampleClass);
   });
 });
