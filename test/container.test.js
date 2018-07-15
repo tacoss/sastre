49c311e8f27c8694fe8f40649c171f30edb14d51
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2018-07-15 17:52:40 -0500

    Add get() impl for Container; WIP

---
+++ test/container.test.js
@@ -0,0 +1,155 @@
+/* eslint-disable no-unused-expressions */
+
+const Container = require('../lib/container');
+const Injector = require('../lib/injector');
+
+const td = require('testdouble');
+const expect = require('chai').expect;
+
+/* global it, describe, beforeEach, afterEach */
+
+describe('Container', () => {
+  describe('static methods', () => {
+    describe('constructor', () => {
+      it('just extends from given args', () => {
+        expect(new Container({ a: 'b' })).to.be.eql({ a: 'b' });
+      });
+    });
+
+    describe('unwrap', () => {
+      let supportsCallback;
+      let bindCallback;
+
+      const props = {
+        foo: {
+          bar: {
+            baz: null,
+          },
+        },
+      };
+
+      beforeEach(() => {
+        supportsCallback = td.func('Injector.supports');
+        bindCallback = td.func('Injector.bind');
+
+        td.replace(Injector, 'supports', supportsCallback);
+        td.replace(Injector, 'bind', bindCallback);
+      });
+
+      afterEach(() => {
+        td.reset();
+      });
+
+      it('should resolve recursively', () => {
+        const result = Container.unwrap(null, props);
+
+        expect(() => {
+          if (result.foo.bar.baz !== null) {
+            throw new Error('Unexpected error');
+          }
+        }).not.to.throw();
+      });
+
+      it('should bind injected properties', () => {
+        td.when(Injector.supports({ baz: null }))
+          .thenReturn(true);
+
+        td.when(Injector.bind(null, { baz: null }))
+          .thenReturn({ baz: 42 });
+
+        const result = Container.unwrap(null, props);
+
+        expect(td.explain(Injector.supports).callCount).to.eql(2);
+        expect(result).to.deep.eql({
+          foo: {
+            bar: {
+              baz: 42,
+            },
+          },
+        });
+      });
+    });
+  });
+
+  describe('instance methods', () => {
+    function buildContainer({ resolverValues, injectableValues } = {}) {
+      return new Container({
+        values: {
+          ...resolverValues,
+        },
+        registry: {
+          ...injectableValues,
+        },
+      });
+    }
+
+    describe('get', () => {
+      afterEach(() => {
+        td.reset();
+      });
+
+      it('should fail on invalid values', () => {
+        const TESTS = [
+          [{}, 'foo', 'undefined'],
+          [{ aNumber: 42 }, 'aNumber', '42'],
+          [{ nullValues: null }, 'nullValues', 'null'],
+          [{ stringValues: 'TEST' }, 'stringValues', "'TEST'"],
+        ];
+
+        TESTS.forEach(testInfo => {
+          const [args, propName, givenValue] = testInfo;
+
+          expect(() => {
+            buildContainer({ resolverValues: args }).get(propName);
+          }).to.throw(`Target '${propName}' is not an object, given ${givenValue}`);
+        });
+      });
+
+      it('should keep locked values as-is', () => {
+        const dep1 = {
+          value: Symbol('UNIQUE_DEPENDENCY'),
+        };
+
+        td.replace(Injector, 'hasLocked', td.func('Injector.hasLocked'));
+        td.when(Injector.hasLocked(dep1))
+          .thenReturn(true);
+
+        expect(buildContainer({ resolverValues: { dep1 } }).get('dep1')).to.deep.eql(dep1);
+      });
+
+      it('should resolve and lock values otherwise', () => {
+        const dep1 = new Injector(({ Value }) => function method() {
+          return new Value();
+        }, {
+          getValue() {},
+        });
+
+        const container = buildContainer({
+          resolverValues: {
+            dep1: {},
+            Value: class {
+              constructor() {
+                this.value = 42;
+              }
+            },
+          },
+          injectableValues: {
+            dep1,
+          },
+        });
+
+        const afterCallback = td.func('decorator.after');
+
+        const resolver = {
+          get: name => container.get(name, afterCallback),
+          ...container,
+        };
+
+        const result = Injector.bind(resolver, dep1);
+
+        expect(result()).to.eql({ value: 42 });
+        expect(td.explain(afterCallback).callCount).to.eql(1);
+      });
+    });
+  });
+});
--- test/injector.test.js
+++ test/injector.test.js
@@ -9,118 +9,120 @@ const td = require('testdouble');
 /* global it, describe, afterEach, beforeEach */
 
 describe('Injector', () => {
-  describe('constructor', () => {
-    it('should fail on invalid values', () => {
-      expect(() => new Injector()).to.throw('Cannot inject non-callable values');
-    });
-  });
-
-  describe('hasLocked', () => {
-    it('detects if values has injectables', () => {
-      expect(Injector.hasLocked({})).to.be.false;
-    });
-  });
-
-  describe('supports', () => {
-    it('validates if values are injectables', () => {
-      expect(Injector.supports()).to.be.false;
-    });
-  });
-
-  describe('assign', () => {
-    it('should fail on unexpected values', () => {
-      expect(() => {
-        Injector.assign();
-      }).to.throw("Target is not an object, given 'undefined'");
+  describe('static methods', () => {
+    describe('constructor', () => {
+      it('should fail on invalid values', () => {
+        expect(() => new Injector()).to.throw('Cannot inject non-callable values');
+      });
     });
 
-    it('can assign and lock injectables once', () => {
-      const target = {};
+    describe('hasLocked', () => {
+      it('detects if values has injectables', () => {
+        expect(Injector.hasLocked({})).to.be.false;
+      });
+    });
 
-      Injector.assign(target, {
-        foo: 'BAR',
+    describe('supports', () => {
+      it('validates if values are injectables', () => {
+        expect(Injector.supports()).to.be.false;
+      });
+    });
+
+    describe('assign', () => {
+      it('should fail on unexpected values', () => {
+        expect(() => {
+          Injector.assign();
+        }).to.throw('Target is not an object, given undefined');
       });
 
-      expect(target).to.be.deep.eql({
-        foo: 'BAR',
+      it('can assign and lock injectables once', () => {
+        const target = {};
+
+        Injector.assign(target, {
+          foo: 'BAR',
+        });
+
+        expect(target).to.be.deep.eql({
+          foo: 'BAR',
+        });
+
+        expect(() => Injector.assign(target, {
+          baz: 'BUZZ',
+        })).to.throw('Cannot assign to locked values');
       });
-
-      expect(() => Injector.assign(target, {
-        baz: 'BUZZ',
-      })).to.throw('Cannot assign to locked values');
     });
-  });
 
-  describe('bind', () => {
-    const someInjectables = {
-      dep1() {},
-      dep2() {
-        return 'OK';
-      },
-    };
-
-    let fakeResolver;
-    let getCallback;
-
-    beforeEach(() => {
-      getCallback = td.func('resolver.get');
-
-      fakeResolver = {
-        get: getCallback,
-        _values: {
-          dep1: {},
-          dep2: {},
+    describe('bind', () => {
+      const someInjectables = {
+        dep1() {},
+        dep2() {
+          return 'OK';
         },
-        _dependencies: {},
       };
 
-      td.when(fakeResolver.get('dep1'))
-        .thenReturn(-42);
-    });
+      let fakeResolver;
+      let getCallback;
 
-    afterEach(() => {
-      td.reset();
-    });
+      beforeEach(() => {
+        getCallback = td.func('resolver.get');
 
-    it('will return plain functions as is', () => {
-      function test() {
-        return 42;
-      }
+        fakeResolver = {
+          get: getCallback,
+          values: {
+            dep1: {},
+            dep2: {},
+          },
+          dependencies: {},
+        };
 
-      const testFn = new Injector(test, someInjectables);
-      const result = Injector.bind(fakeResolver, testFn)();
+        td.when(fakeResolver.get('dep1'))
+          .thenReturn(-42);
+      });
 
-      expect(result).to.eql(42);
-    });
+      afterEach(() => {
+        td.reset();
+      });
 
-    it('will return instantiated classes bound', () => {
-      class Test {
-        constructor({ dep1 }) {
-          this.dep = dep1;
+      it('will return plain functions as is', () => {
+        function test() {
+          return 42;
         }
-      }
 
-      const testClass = new Injector(Test, someInjectables);
-      const result = Injector.bind(fakeResolver, testClass);
+        const testFn = new Injector(test, someInjectables);
+        const result = Injector.bind(fakeResolver, testFn)();
 
-      expect(result).to.deep.eql({ dep: -42 });
-      expect(td.explain(getCallback).callCount).to.eql(1);
-    });
+        expect(result).to.eql(42);
+      });
 
-    it('arrow functions are used to inject values', () => {
-      const testArrow = new Injector(({ dep1 }) => () => dep1, someInjectables);
-      const result = Injector.bind(fakeResolver, testArrow);
+      it('will return instantiated classes bound', () => {
+        class Test {
+          constructor({ dep1 }) {
+            this.dep = dep1;
+          }
+        }
 
-      expect(result()).to.eql(-42);
-      expect(td.explain(getCallback).callCount).to.eql(1);
-    });
+        const testClass = new Injector(Test, someInjectables);
+        const result = Injector.bind(fakeResolver, testClass);
 
-    it('would return values given from injectors (if any)', () => {
-      const testArrow = new Injector(({ dep2 }) => () => dep2, someInjectables);
-      const result = Injector.bind(fakeResolver, testArrow);
+        expect(result).to.deep.eql({ dep: -42 });
+        expect(td.explain(getCallback).callCount).to.eql(1);
+      });
 
-      expect(result()).to.eql('OK');
-      expect(td.explain(getCallback).callCount).to.eql(0);
+      it('arrow functions are used to inject values', () => {
+        const testArrow = new Injector(({ dep1 }) => () => dep1, someInjectables);
+        const result = Injector.bind(fakeResolver, testArrow);
+
+        expect(result()).to.eql(-42);
+        expect(td.explain(getCallback).callCount).to.eql(1);
+      });
+
+      it('would return values given from injectors (if any)', () => {
+        const testArrow = new Injector(({ dep2 }) => () => dep2, someInjectables);
+        const result = Injector.bind(fakeResolver, testArrow);
+
+        expect(result()).to.eql('OK');
+        expect(td.explain(getCallback).callCount).to.eql(0);
+      });
     });
   });
 });
