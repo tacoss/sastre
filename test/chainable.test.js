848d3857a3451bd421d959da5d51064a49e0ecb3
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2018-07-25 20:10:53 -0500

    New Chainable DSL

--- test/chainable.test.js
+++ test/chainable.test.js
@@ -12,11 +12,11 @@ describe('Chainable', () => {
   describe('static methods', () => {
     describe('constructor', () => {
       it('should validate its input', () => {
-        expect(() => new Chainable().call()).to.throw('Invalid middleware, given undefined');
+        expect(() => new Chainable()).to.throw('Invalid middleware, given undefined');
       });
 
       it('should validate each given function', () => {
-        expect(() => new Chainable(null, { undef: null }).call()).to.throw('Invalid function, given null');
+        expect(() => new Chainable(null, { undef: null })).to.throw('Invalid function, given null');
       });
     });
 
@@ -27,76 +27,103 @@ describe('Chainable', () => {
     });
   });
 
-  describe('instance methods', () => {
-    describe('call', () => {
-      it('should invoke given middlewares in order', async () => {
-        let values;
-        let callCount;
+  describe('callable', () => {
+    it('should inject chainable methods on call', async () => {
+      const callable = td.func();
 
-        function delay(ms, cb) {
-          return new Promise(resolve => setTimeout(() => resolve(cb()), ms));
-        }
-
-        function reset() {
-          values = undefined;
-          callCount = undefined;
-        }
-
-        function push(value) {
-          values = values || [];
-          values.push(value);
-
-          callCount = callCount || 0;
-          callCount += 1;
-        }
-
-        const test = new Chainable(null, {
-          a: () => push('A'),
-          b: () => push('B'),
-          c: () => delay(100, () => push('C')),
-        });
-
-        reset();
-        await test.call();
-        expect(values).to.be.undefined;
-        expect(callCount).to.be.undefined;
-
-        reset();
-        await test.call.a();
-        expect(callCount).to.eql(1);
-        expect(values).to.eql(['A']);
-
-        reset();
-        await test.call.a.c.b();
-        expect(callCount).to.eql(3);
-        expect(values).to.eql(['A', 'C', 'B']);
+      const test = new Chainable(null, {
+        a: callable,
+        b: callable,
+        c: callable,
       });
 
-      it('can be called through container.get() resolution', async () => {
-        const afterCallback = td.func();
-        const callable = td.func();
+      await test(({ a }) => a.b.c)();
 
-        const container = new Container(null, {
-          values: {
-            dep1: new Chainable(null, {
-              a: callable,
-              b: callable,
-              c: callable,
-            }),
-          },
-          registry: {
-            dep1: {},
-          },
-        });
+      expect(td.explain(callable).callCount).to.eql(3);
+    });
 
-        await container.get('dep1', afterCallback)();
-        await container.get('dep1', afterCallback).a();
-        await container.get('dep1', afterCallback).a.b();
-        await container.get('dep1', afterCallback).b.b.c();
+    it('should invoke given middlewares in order', async () => {
+      let values;
+      let callCount;
 
-        expect(td.explain(callable).callCount).to.eql(6);
-        expect(td.explain(afterCallback).callCount).to.eql(0);
+      const inc = td.func();
+
+      td.when(inc('A')).thenReturn(1);
+      td.when(inc('B')).thenReturn(2);
+      td.when(inc('C')).thenReturn(3);
+
+      function delay(ms, cb) {
+        return new Promise(resolve => setTimeout(() => resolve(cb()), ms));
+      }
+
+      function reset() {
+        values = undefined;
+        callCount = undefined;
+      }
+
+      function push(value) {
+        values = values || [];
+        values.push(value);
+
+        callCount = callCount || 0;
+        callCount += inc(value);
+      }
+
+      const test = new Chainable(null, {
+        a: () => push('A'),
+        b: () => push('B'),
+        c: () => delay(100, () => push('C')),
       });
+
+      reset();
+      expect(test()).to.throw('Missing middleware to chain');
+      expect(values).to.be.undefined;
+      expect(callCount).to.be.undefined;
+      expect(td.explain(inc).callCount).to.eql(0);
+
+      reset();
+      await test(({ a }) => a)();
+      expect(callCount).to.eql(1);
+      expect(values).to.eql(['A']);
+      expect(td.explain(inc).callCount).to.eql(1);
+
+      reset();
+      await test(({ a }) => a.b)();
+      expect(callCount).to.eql(3);
+      expect(values).to.eql(['A', 'B']);
+      expect(td.explain(inc).callCount).to.eql(3);
+
+      reset();
+      await test(({ a }) => a.c.b)();
+      expect(callCount).to.eql(6);
+      expect(values).to.eql(['A', 'C', 'B']);
+      expect(td.explain(inc).callCount).to.eql(6);
+    });
+
+    it('can be called through container.get() resolution', async () => {
+      const after = td.func();
+      const callable = td.func();
+
+      const container = new Container(null, {
+        values: {
+          dep1: new Chainable(null, {
+            a: callable,
+            b: callable,
+            c: callable,
+          }),
+        },
+        registry: {
+          dep1: {},
+        },
+      });
+
+      await container.get('dep1', after)();
+      await container.get('dep1')(({ a }) => a)();
+      await container.get('dep1')(({ a }) => a.b)();
+      await container.get('dep1')(({ a }) => a.b.c)();
+
+      expect(td.explain(after).callCount).to.eql(1);
+      expect(td.explain(callable).callCount).to.eql(6);
     });
   });
 });
