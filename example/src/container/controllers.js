59c169fa9ad230ae56dd1256b9bb1bdac29cdb52
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2019-02-17 10:33:45 -0600

    Always return definition

--- example/src/container/controllers.js
+++ example/src/container/controllers.js
@@ -3,7 +3,9 @@ const { Resolver } = require('@lib'); // eslint-disable-line
 class ControllersResolver {
   constructor(container, controllersDir) {
     return new Resolver(container, controllersDir, (name, definition) => {
-      console.log('Custom logic here, e.g. GRPC IoC (aka front-controller)', name, definition);
+      console.log('Custom logic here, e.g. GRPC IoC (aka front-controller)', name);
+
+      return definition;
     });
   }
 }
